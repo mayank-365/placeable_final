f7a0df864c8f0ef2222253e4729bc3afc5c44434
mayank-365 <mayankkashyap365@gmail.com>
2021-10-20 05:32:21 +0530

    academics included

---
+++ app/src/main/java/com/example/placeable_final/basic_detail.java
@@ -0,0 +1,86 @@
+package com.example.placeable_final;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+
+public class basic_detail extends Fragment  {
+    View view;
+    Button Next;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_basic_detail, container, false);
+
+        //
+        final Calendar myCalendar= Calendar.getInstance();
+        Next=view.findViewById(R.id.basic_details_next);
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    acadmeics_details adetails=new acadmeics_details();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment1,new acadmeics_details(),"ACADEMICS_DETAILS");
+                    fragmentTransaction.addToBackStack("ACADEMICS_DETAILS");
+                    fragmentTransaction.commit();
+                }
+
+
+        });
+       EditText edittext= (EditText)view.findViewById(R.id.Basic_details_dob);
+       DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+           @Override
+           public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                 int dayOfMonth) {
+               // TODO Auto-generated method stub
+               myCalendar.set(Calendar.YEAR, year);
+               myCalendar.set(Calendar.MONTH, monthOfYear);
+               myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+               updateLabel(myCalendar);
+           }
+
+       };
+
+       edittext.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v) {
+               // TODO Auto-generated method stub
+               new DatePickerDialog(getContext(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+           }
+       });
+       //
+
+        return view;
+   }
+    private void updateLabel(Calendar myCalendar) {
+        String myFormat = "dd/yy/MM";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        EditText edittext=view.findViewById(R.id.Basic_details_dob);
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+}
--- app/src/main/java/com/example/placeable_final/company.java
+++ app/src/main/java/com/example/placeable_final/company.java
@@ -1,21 +1,51 @@
 package com.example.placeable_final;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class company extends AppCompatActivity {
+public class company extends Fragment {
+    View view;
+    Button reg_comp;
+    Button open_comp;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_company, container, false);
 
+        reg_comp = view.findViewById(R.id.registered_comp);
+        open_comp = view.findViewById(R.id.open_companies);
+
+        reg_comp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.companyframe,new registered_companies(),"REGISTERED_COMPANIES");
+                fragmentTransaction.addToBackStack("REGISTERED_COMPANIES");
+                fragmentTransaction.commit();
+            }
+        });
+        open_comp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.companyframe,new open_companies(),"OPEN_COMPANIES");
+                fragmentTransaction.addToBackStack("OPEN_COMPANIES");
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        return view;
     }
 }
