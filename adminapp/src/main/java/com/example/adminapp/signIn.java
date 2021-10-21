c52139f7245d1f08a08124d54ae3a8d982b003b3
Abhay Malik <84030691+abhay-malik@users.noreply.github.com>
2021-10-21 11:14:26 +0530

    admin app module added.

---
+++ adminapp/src/main/java/com/example/adminapp/signIn.java
@@ -0,0 +1,23 @@
+package com.example.adminapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class signIn extends Fragment {
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        return view;
+    }
+}
